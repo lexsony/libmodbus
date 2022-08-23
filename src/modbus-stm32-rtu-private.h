a58b6aed1b83905147e8c9348ad3fc56dab6ab7f
yonzkon <yonzkon@gmail.com>
2022-08-23 22:25:47 +0800

    Copy modbus-rtu* to modbus-stm32-rtu*, and delete code not used

---
+++ src/modbus-stm32-rtu-private.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright © Stéphane Raimbault <stephane.raimbault@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#ifndef MODBUS_STM32_RTU_PRIVATE_H
+#define MODBUS_STM32_RTU_PRIVATE_H
+
+#include <stdint.h>
+
+#define _MODBUS_RTU_HEADER_LENGTH      1
+#define _MODBUS_RTU_PRESET_REQ_LENGTH  6
+#define _MODBUS_RTU_PRESET_RSP_LENGTH  2
+
+#define _MODBUS_RTU_CHECKSUM_LENGTH    2
+
+typedef struct _modbus_rtu {
+    /* Device: "/dev/ttyS0", "/dev/ttyUSB0" or "/dev/tty.USA19*" on Mac OS X. */
+    char *device;
+    /* Bauds: 9600, 19200, 57600, 115200, etc */
+    int baud;
+    /* Data bit */
+    uint8_t data_bit;
+    /* Stop bit */
+    uint8_t stop_bit;
+    /* Parity: 'N', 'O', 'E' */
+    char parity;
+    /* To handle many slaves on the same link */
+    int confirmation_to_ignore;
+} modbus_rtu_t;
+
+#endif /* MODBUS_STM32_RTU_PRIVATE_H */
